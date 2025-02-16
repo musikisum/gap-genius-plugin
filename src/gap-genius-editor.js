bf1a109e48f7340ee2197b65174b96eace1eafac
Ulrich Kaiser <ulrich.kaiser@hmtm.de>
2025-02-16 18:52:33 +0100

    Update text function

--- src/gap-genius-editor.js
+++ src/gap-genius-editor.js
@@ -13,6 +13,7 @@ import { FORM_ITEM_LAYOUT, FORM_ITEM_LAYOUT_WITHOUT_LABEL } from '@educandu/educ
 export default function GapGeniusEditor({ content, onContentChanged }) {
 
   const { analyseText, text, width, replacements } = content;
+  console.log('replacements:', replacements)
   const { t } = useTranslation('musikisum/educandu-plugin-gap-genius');
 
   const updateContent = newContentValues => {
@@ -29,43 +30,51 @@ export default function GapGeniusEditor({ content, onContentChanged }) {
     updateContent({ text: et, replacements: nro });
   };
 
-  const onAnalyseButtonClick = () => {
-    updateContent({ analyseText: !analyseText });
-  };
-
   const onTextChange = event => {
     const newText = event.target.value;
     updateContent({ text: newText });
   };
 
-  const onReplacementsChange = (event, itemIndex) => {
-    console.log('onReplacementsChange', event, itemIndex)
-    // updateContent({ replacements: replacementsCopy });
+  const onModusChange = () => {
+    const nro = GapGeniusUtils.createNewReplacementObjects(text);
+    updateContent({ text, analyseText: !analyseText, replacements: nro });
+  };
+
+  const onTextSaveChange = () => {
+    const nro = GapGeniusUtils.createNewReplacementObjects(text);
+    const newText = GapGeniusUtils.updateTextWithSynonyms(text, nro);
+    updateContent({ text: newText, replacements: nro });
+  };
+
+  const onReplacementsChange = item => {
+    const updatedReplacementsCopy = cloneDeep(replacements).map(obj => obj.index === item.index ? { ...obj, list: item.list } : obj);
+    const newText = GapGeniusUtils.updateTextWithSynonyms(text, updatedReplacementsCopy);
+    updateContent({ text: newText, replacements: updatedReplacementsCopy });
   };
 
   return (
-    <div className="EP_Educandu_Gapgenius_Editor">
+    <div className="EP_Educandu_Gap_Genius_Editor">
       <Form labelAlign="left">
         <Form.Item label={t('gapText')} {...FORM_ITEM_LAYOUT}>
-          <div style={{ color: 'red !important' }}>
-            <MarkdownInput value={text} onChange={onTextChange} color="red" renderAnchors disabled={analyseText} />
-          </div>
+          <MarkdownInput value={text} onChange={onTextChange} renderAnchors disabled={analyseText} className='defaultTextColor' />
         </Form.Item>
         <Form.Item {...FORM_ITEM_LAYOUT_WITHOUT_LABEL}>
           <Flex options={['center']} gap='middle'>
+            <Button type='primary' className={analyseText ? 'btuttonColorVariables' : 'btuttonColorText'} onClick={onModusChange}>{analyseText ? t('keywordsInputMode'): t('textInputMode')}</Button>
+            <Button type='primary' onClick={onTextSaveChange}>{t('actualizeText')}</Button>
             <Button type='primary' onClick={onExampleButtonClick}>{t('insertText')}</Button>
-            <Button type='primary' onClick={onAnalyseButtonClick}>{analyseText ? t('keywordsInputMode'): t('textInputMode')}</Button>
           </Flex>
         </Form.Item>
-        { !analyseText
+        { analyseText ? <div className='messageArea' >{t('errorText')}</div> : null }
+        { analyseText
           ? replacements.map(item => {
             return (
-              <Form.Item key={item.index} label={`${item.index}. ${item.list[0]}`} {...FORM_ITEM_LAYOUT}>
+              <Form.Item key={item.index + 1} label={`${item.index}. ${item.list[0]}`} {...FORM_ITEM_LAYOUT}>
                 <EditableInput value={item} onSave={event => onReplacementsChange(event, item.index)} />
               </Form.Item>
             );
           })
-          : <div style={{ textAlign: 'center', marginTop: '20px', marginBottom: '40px', color: 'red', fontWeight: 'bold' }}>{t('errorText')}</div>}
+          : null}
         <Form.Item
           label={<Info tooltip={t('common:widthInfo')}>{t('common:width')}</Info>}
           {...FORM_ITEM_LAYOUT}
--- src/gap-genius-utils.js
+++ src/gap-genius-utils.js
@@ -1,5 +1,3 @@
-import cloneDeep from '@educandu/educandu/utils/clone-deep.js';
-
 const exampleText = 'Carl Dahlhaus hat die Begriffe Akkord und Klang sehr scharfsinnig definiert. Man spricht von einem (Akkord)(), wenn man einen Grundton hört, hören wir keinen Grundton, wird von einem (Klang)(Geräusch) gesprochen. Nach dieser Definitionen ist unser musikalisches (Hören)(Wahrnehmen, Denken, Vorstellen) entscheidend für die Verwendung der Begriffe. Man kann (zwei)(zwei) Töne als Akkord hören, (drei)() Töne in Terzschichtung dagegen auch als (Klang)(). Aus dieser Perspektive ist der Ausdruck C-Dur-Dreiklang (falsch)(ungenau, schlecht, zu vermeiden), denn es müsste (C-Dur-Akkord)() heißen, weil der Ausdruck aussagt, dass wir (C)() als Grundton wahrnehmen. Ist die Definition so gut, dass es sich lohnt, gegen (Windmühlen)(die Masse, Vorurteile, ungenaues Denken, alle, die meisten) anzukämpfen?';
 
 const _regex = /\((?<expression>[^()]+)\)\((?<list>[^()]*)\)/g;
@@ -8,17 +6,32 @@ const createNewReplacementObjects = text => {
   let index = 0;
   const obj = [];
   for (const match of text.matchAll(_regex)) {
-    index += 1;
     const expression = match.groups.expression;
-    const list = match.groups.list.split(/[;,]\s*/).filter(item => item !== '');
+    const list = match.groups.list.split(/[;]\s*/).filter(item => item !== '');
     list.unshift(expression);
     obj.push({ index, expression, list: [...new Set(list)] });
+    index += 1;
   };
   return obj;
 };
 
+function updateTextWithSynonyms(text, replacements) {
+  if (!text) {
+    return 'Ein interner Fehler ist aufgetreten, Entschuldigung!';
+  }
+  let matchIndex = 0;
+  return text.replace(_regex, (match, expression) => {
+    const replacementObj = replacements[matchIndex];
+    matchIndex += 1;
+    return replacementObj && replacementObj.expression === expression 
+      ? `(${expression})(${replacementObj.list.join('; ')})`
+      : match;
+  });
+}
+
 const GapGeniusUtils = {
   exampleText,
+  updateTextWithSynonyms,
   createNewReplacementObjects
 };
 
